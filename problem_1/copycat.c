92ca2348cf24123ca61f7a6ba370809f53f4f9ae
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-09-05 17:19:36 -0400

    copycat initial commit

---
+++ problem_1/copycat.c
@@ -0,0 +1,11 @@
+#include "copycat.h"
+
+#include "unistd.h" // getopt
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
+
+int parse_args(int buffersize, char outfile[], char* infiles[]) {
+	return 0;
+}
---
+++ problem_1/copycat.h
@@ -0,0 +1,6 @@
+#ifndef COPYCAT_H
+#define COPYCAT_H
+
+int parse_args(int buffersize, char outfile[], char* infiles[]);
+
+#endif /* COPYCAT_H */
