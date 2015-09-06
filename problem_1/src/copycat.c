d494291f9d9b484e10d7d0fbaff0b732139f2e74
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-09-06 18:15:28 -0400

    Refactor and update usage

---
+++ problem_1/src/copycat.c
@@ -0,0 +1,11 @@
+#include "copycat.h"
+
+int main(int argc, char* argv[]) {
+    Options options;
+    
+    cc_parse_args(argc, argv, &options);
+    cc_log(&options);
+    cc_copy(&options);
+
+    return 0;
+}
---
+++ problem_1/src/copycat.h
@@ -0,0 +1,45 @@
+#ifndef COPYCAT_H
+#define COPYCAT_H
+
+#define DEFAULT_BUFFER_SIZE 1024 // 1KB
+#define DEFAULT_FILE_PERM   0664 // RW-RW-R--
+
+typedef struct {
+    char**       argv;
+    int          argc;
+    unsigned int buffersize;
+    int          outfile_index;
+    int          infiles_index;
+    int          verbose;
+    unsigned int mode;
+} Options;
+
+typedef enum {
+    CC_NONE,
+    CC_USAGE,
+    CC_MALLOC_FAIL
+} cc_error_t;
+
+typedef enum {
+    CC_F_NONE,
+    CC_F_OPEN_RD,
+    CC_F_OPEN_WR,
+    CC_F_READ,
+    CC_F_WRITE
+} cc_file_error_t;
+
+
+
+cc_error_t      cc_parse_args(int argc, char* argv[], Options* options);
+
+void            cc_error(cc_error_t e);
+
+void            cc_error_f(cc_file_error_t e, int err, char filename[]);
+
+void            cc_log(Options* options);
+
+void            cc_copy(Options* options);
+
+cc_file_error_t cc_copy_file(const int fo, const int fi, const unsigned int buf_len, char* buf, int* err);
+
+#endif /* COPYCAT_H */
