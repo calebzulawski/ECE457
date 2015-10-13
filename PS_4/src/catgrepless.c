c44e2de202a4a30b759df99136008772e4291b93
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-10-12 21:31:22 -0400

    Fix order of grep/more

--- PS_4/src/catgrepless.c
+++ PS_4/src/catgrepless.c
@@ -51,63 +51,64 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
-	int pipefd1[2], pipefd2[2];
-
-	if ( pipe(pipefd1) == -1 || pipe(pipefd2) == -1){
-		fprintf(stderr, "Failed to open pipe: %s\n", strerror(errno));
-		exit(-1);
-	}
-
-	int greppid;
-	switch (greppid = fork()) {
-		case 0:
-			dup2_msg(pipefd1[0], STDIN_FILENO);
-			dup2_msg(pipefd2[1], STDOUT_FILENO);
-			close_msg(pipefd1[0]);
-			close_msg(pipefd1[1]);
-			close_msg(pipefd2[0]);
-			close_msg(pipefd2[1]);
-			execlp("grep", "grep", argv[1], (char *) NULL);
-			fprintf(stderr, "Failed to open grep: %s\n", strerror(errno));
-			exit(-1);
-		case -1:
-			fprintf(stderr, "Failed to fork() for grep: %s\n", strerror(errno));
-			exit(-1);
-		default:
-			break;
-	}
-
-	switch (fork()) {
-		case 0:
-			dup2_msg(pipefd2[0], STDIN_FILENO);
-			close_msg(pipefd1[0]);
-			close_msg(pipefd1[1]);
-			close_msg(pipefd2[0]);
-			close_msg(pipefd2[1]);
-			execlp("less", "less", (char *) NULL);
-			fprintf(stderr, "Failed to open more: %s\n", strerror(errno));
-			exit(-1);
-		case -1:
-			fprintf(stderr, "Failed to fork() for more: %s\n", strerror(errno));
-			exit(-1);
-		default:
-			break;
-	}
-	close_msg(pipefd1[0]);
-	close_msg(pipefd2[0]);
-	close_msg(pipefd2[1]);
-	int fi;
-	char buf [COPYBUFSIZ];
 	for (int i = 2; i < argc; i++) {
+		int pipefd1[2], pipefd2[2];
+
+		if ( pipe(pipefd1) == -1 || pipe(pipefd2) == -1){
+			fprintf(stderr, "Failed to open pipe: %s\n", strerror(errno));
+			exit(-1);
+		}
+
+		int greppid;
+		switch (greppid = fork()) {
+			case 0:
+				dup2_msg(pipefd1[0], STDIN_FILENO);
+				dup2_msg(pipefd2[1], STDOUT_FILENO);
+				close_msg(pipefd1[0]);
+				close_msg(pipefd1[1]);
+				close_msg(pipefd2[0]);
+				close_msg(pipefd2[1]);
+				execlp("grep", "grep", argv[1], (char *) NULL);
+				fprintf(stderr, "Failed to open grep: %s\n", strerror(errno));
+				exit(-1);
+			case -1:
+				fprintf(stderr, "Failed to fork() for grep: %s\n", strerror(errno));
+				exit(-1);
+			default:
+				break;
+		}
+
+		switch (fork()) {
+			case 0:
+				dup2_msg(pipefd2[0], STDIN_FILENO);
+				close_msg(pipefd1[0]);
+				close_msg(pipefd1[1]);
+				close_msg(pipefd2[0]);
+				close_msg(pipefd2[1]);
+				execlp("less", "less", (char *) NULL);
+				fprintf(stderr, "Failed to open more: %s\n", strerror(errno));
+				exit(-1);
+			case -1:
+				fprintf(stderr, "Failed to fork() for more: %s\n", strerror(errno));
+				exit(-1);
+			default:
+				break;
+		}
+		close_msg(pipefd1[0]);
+		close_msg(pipefd2[0]);
+		close_msg(pipefd2[1]);
+		int fi;
+		char buf [COPYBUFSIZ];
+
 		if ((fi = open(argv[i], O_RDONLY)) == -1) {
 			fprintf(stderr, "Failed to open %s: %s\n", argv[i], strerror(errno));
 			continue;
 		}
 		copy_file(fi, pipefd1[1], buf, COPYBUFSIZ);
+		close_msg(pipefd1[1]);
+		wait(0);
+		wait(0);
 	}
-	close_msg(pipefd1[1]);
 
-	wait(0);
-	wait(0);
 	return 0;
 }
