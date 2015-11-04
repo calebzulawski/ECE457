6c8531f238e83c8ac6817539d8e23d0f8a468618
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-11-04 02:07:23 -0500

    Add tests A-E, with E not quite working

--- PS_5/src/mtest.c
+++ PS_5/src/mtest.c
@@ -12,6 +12,9 @@
 #define _GNU_SOURCE
 
 const char *smallfile = "small.file";
+const char *largefile = "large.file";
+
+const char *data = "some test data";
 
 void sig_handle(int s) {
 	psignal(s, "Caught signal");
@@ -44,6 +47,27 @@ int _open(const char *pathname, int flags) {
 	return fd;
 }
 
+off_t _lseek(int fd, off_t offset, int whence) {
+	off_t o;
+	if ((o = lseek(fd, offset, whence)) == -1)
+		quit("lseek() failed");
+	return o;
+}
+
+ssize_t _read(int fd, void *buf, size_t count) {
+	ssize_t s;
+	if ((s = read(fd, buf, count)) == -1)
+		quit("read() failed");
+	return s;
+}
+
+ssize_t _write(int fd, const void *buf, size_t count) {
+	ssize_t s;
+	if ((s = write(fd, buf, count)) == -1)
+		quit("write() failed");
+	return s;
+}
+
 void test_A() {
 	printf("Opening small file\n");
 	int fd = _open(smallfile, O_RDONLY);
@@ -54,12 +78,67 @@ void test_A() {
 	sprintf(m, "a");
 }
 
+void test_BC(int shared) {
+	size_t offset = 0;
+	printf("Opening test file\n");
+	int fd = _open(largefile, O_RDWR);
+	size_t length = file_size(fd);
+	printf("File has length %zuB\n", length);
+	void *m = _mmap(NULL, length, PROT_READ|PROT_WRITE, (shared ? MAP_SHARED : MAP_PRIVATE), fd, offset);
+	printf("Writing the following to mapped file: %s\n", data);
+	sprintf(m, "%s", data);
+	_lseek(fd, offset, SEEK_SET);
+	char buf [32];
+	_read(fd, buf, strlen(data));
+	printf("Read the following from file: %s\n", buf);
+	printf("Data matched: %s\n", strcmp(data, buf) == 0 ? "yes" : "no");
+}
+
+void test_DE(int e) {
+	size_t offset = 0;
+	printf("Opening test file\n");
+	int fd = _open(largefile, O_RDWR);
+	size_t length = file_size(fd);
+	printf("File has length %zuB\n", length);
+	void *m = _mmap(NULL, length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, offset);
+	printf("Writing one byte past end of file\n");
+	sprintf(m + length, "%s", data);
+	size_t newlength = file_size(fd);
+	printf("File now has length %zuB\n", newlength);
+	printf("File expanded: %s\n", newlength == length ? "no" : "yes");
+
+	if (e) {
+		char buf[32];
+		printf("Expanding file\n");
+		_lseek(fd, strlen(data), SEEK_END);
+		_write(fd, '\0', 1);
+		_lseek(fd, length, SEEK_SET);
+		_read(fd, buf, 32);
+		newlength = file_size(fd);
+		printf("File now has length %zuB\n", newlength);
+		printf("Data read from file: %s\n", buf);
+		printf("Data was written to file: %s", strcmp(buf, data) == 0 ? "yes" : "no");
+	}
+}
+
 int main( int argc, const char* argv[] ) {
 	if (signal(SIGSEGV,sig_handle) == SIG_ERR)
 		quit("signal() failed");
 	switch (argv[1][0]) {
 		case 'A':
 			test_A();
+			break;
+		case 'B':
+			test_BC(1);
+			break;
+		case 'C':
+			test_BC(0);
+			break;
+		case 'D':
+			test_DE(0);
+			break;
+		case 'E':
+			test_DE(1);
 			break;
 	}
 }
