34a338cb559aa1c991ac0bbcaf7bb76056c01aee
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-12-06 15:53:30 -0500

    Test almost works

--- PS_9/src/sched_test.c
+++ PS_9/src/sched_test.c
@@ -1,25 +1,43 @@
 #include "sched.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void child() {
+	printf("In child with pid %d, ppid %d\n", sched_getpid(), sched_getppid());
+	unsigned long max = 1e8;
+	for (unsigned long i = 0; i < max; i++) {
+		getpid();
+	}
+	sched_exit(sched_getpid());
+}
 
 void init() {
 	printf("Hello\n");
 	int pid;
-	switch(pid = sched_fork()) {
-		case -1:
-			printf("sched_fork() error\n");
-			sched_exit(0);
-		case 0:
-			printf("In child with pid %d, ppid %d\n", sched_getpid(), sched_getppid());
-			sched_exit(7);
-		default:
-			printf("In parent with pid %d, child pid %d\n", sched_getpid(), pid);
-			int code;
-			sched_wait(&code);
-			printf("Child returned with code %d\n", code);
-			sched_exit(0);
+	for (int i = 0; i < 5; i++) {
+		switch(pid = sched_fork()) {
+			case -1:
+				printf("sched_fork() error\n");
+				sched_exit(0);
+			case 0:
+				child();
+			default: 
+				printf("Created pid %d\n", pid);
+				break;
+		}	
 	}
-	printf("Ending init process... bad things may happen\n");
+	printf("In parent with pid %d, child pid %d\n", sched_getpid(), pid);
+	int code;
+	for (int i = 0; i < 5; i++) {
+		sched_wait(&code);
+		printf("Child returned with code %d\n", code);
+	}
+	sched_exit(0);
+	printf("Ending init process...\n");
+	exit(0);
 }
 
 int main(int argc, char **argv) {
